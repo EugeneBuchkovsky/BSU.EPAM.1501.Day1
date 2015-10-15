abc4948b6ad809baa6c30415836c79a14d5bfd33
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-15 13:44:38 +0200

    Task2

--- BSU.EPAM.1501.Day1.Bychkovsky.Task2/PreparationForSort.cs
+++ BSU.EPAM.1501.Day1.Bychkovsky.Task2/PreparationForSort.cs
@@ -25,22 +25,6 @@ namespace BSU.EPAM._1501.Day1.Bychkovsky.Task2
         }
 
         /// <summary>
-        /// finding the maximum element of the rows source array.
-        /// </summary>
-        /// <param name="sourceArray"></param>
-        /// <returns>int[]</returns>
-        public static int[] MaxElement(int[][] sourceArray)
-        {
-            int[] finalArray = new int[sourceArray.Length];
-            for (var i = 0; i < finalArray.Length; i++)
-            {
-                if (sourceArray[i] != null)
-                    finalArray[i] = sourceArray[i].Max();
-            }
-            return finalArray;
-        }
-
-        /// <summary>
         /// finding the minimum element of the rows source array.
         /// </summary>
         /// <param name="sourceArray"></param>
@@ -55,5 +39,21 @@ namespace BSU.EPAM._1501.Day1.Bychkovsky.Task2
             }
             return finalArray;
         }
+
+        /// <summary>
+        /// finding the maximum element of the rows source array.
+        /// </summary>
+        /// <param name="sourceArray"></param>
+        /// <returns>int[]</returns>
+        public static int[] MaxElement(int[][] sourceArray)
+        {
+            int[] finalArray = new int[sourceArray.Length];
+            for (var i = 0; i < finalArray.Length; i++)
+            {
+                if (sourceArray[i] != null)
+                    finalArray[i] = sourceArray[i].Max();
+            }
+            return finalArray;
+        }        
     }
 }
