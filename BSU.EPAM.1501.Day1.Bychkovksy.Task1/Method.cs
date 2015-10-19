d00715a82ee209ba623da1c48363401c1c32ae2d
EugeneBuchkovsky <buchkovsyeugene20th@gmail.com>
2015-10-19 23:07:41 +0200

    Test

--- BSU.EPAM.1501.Day1.Bychkovksy.Task1/Method.cs
+++ BSU.EPAM.1501.Day1.Bychkovksy.Task1/Method.cs
@@ -8,15 +8,19 @@ namespace BSU.EPAM._1501.Day1.Bychkovksy.Task1
 {
     public class Method
     {
-        private readonly static double eps = 0.00001;
         /// <summary>
         /// calculating the roots of Newton's method
         /// </summary>
         /// <param name="number"></param>
         /// <param name="pow"></param>
         /// <returns>double</returns>
-        public static double Newton(double number, double pow)
+        public static double Newton(double number, double pow, double eps)
         {
+            if ((pow < 0) || ((pow % 2 == 0) && (number < 0)))
+                return double.NaN;
+            if ((eps > 1) || (eps < 0))
+                //throw new IncorrectEpsilonException("Epsilon outside the allowable values");
+                throw new Exception("Epsilon outside the allowable values");
             double x = ((pow - 1) * number + number / Math.Pow(number, pow - 1)) / pow;
             double xn;
             double temp;
---
+++ BSU.EPAM.1501.Day1.Bychkovsky.Task1.Test/MethodTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BSU.EPAM._1501.Day1.Bychkovksy.Task1;
+
+namespace BSU.EPAM._1501.Day1.Bychkovsky.Task1.Test
+{
+    [TestClass]
+    public class MethodTest
+    {
+        [TestMethod]
+        public void Newton_16pow4_2returned()
+        {
+            var ExpectedResult = Math.Pow(16,0.25);
+
+            Assert.AreEqual(ExpectedResult, Math.Round(Method.Newton(16, 4, 0.00001), 3));
+        }
+
+        [TestMethod]
+        public void Newton_minus100pow2_NANreturned()
+        {
+            double ExpectedResult = double.NaN;
+
+            Assert.AreEqual(ExpectedResult, Method.Newton(-100, 2, 0.00001));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Newton_eps_errorreturned()
+        {
+            Exception error = new Exception();
+
+            Assert.AreEqual(error, Method.Newton(100, 2, 2));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void Newton_minuseps_errorreturned()
+        {
+            Exception error = new Exception();
+
+            Assert.AreEqual(error, Method.Newton(100, 2, -5));
+        }
+    }
+}
